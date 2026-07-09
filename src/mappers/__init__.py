3f5644199f4ff24a46b4399abfab6f9aaed5c974
xavierjzsong-cp <jinze.s@completionproducts.com>
2026-07-09 14:10:06 +0800

    Add TSH mapper

--- src/mappers/__init__.py
+++ src/mappers/__init__.py
@@ -1,9 +1,11 @@
 """Partner input mappers."""
 
 from src.mappers.base_mapper import BaseMapper
+from src.mappers.tsh_mapper import TshMapper
 from src.mappers.vam_mapper import VamMapper
 
 __all__ = [
     "BaseMapper",
+    "TshMapper",
     "VamMapper",
 ]
